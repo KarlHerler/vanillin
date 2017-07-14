0f08dac874b98f5769d1e3fcabaa010c2c0c9c4b
Karl Herler <hi@karlherler.com>
2017-07-14 15:42:10 +0300

    Added an intial hyper implementation of getting data from vanillagaming

---
+++ src/vanillagaming/mod.rs
@@ -0,0 +1,67 @@
+// vanillagaming curler
+// do curl http://db.vanillagaming.org/?search=[urlencodded thing]
+// if response is 302, use link
+// if 200 check if nothing found, return "nothing found, sorry"
+// if 200 check if many things found, return that
+use std::io::{self, Write};
+use std::string::String;
+use futures::{Future, Stream, IntoFuture};
+use futures::future;
+use hyper::{Client, Request, Method, Error};
+use tokio_core::reactor::Core;
+
+
+pub fn find(thing: &str) -> Option<String> {
+  println!("Vanillagaming::find called with: {}", thing);
+  let origin = "http://db.vanillagaming.org/";
+
+  // TODO: Perhaps move this initialization into a module for perf
+  let mut core = Core::new().unwrap();
+  let client = Client::new(&core.handle());
+
+
+  let uri_str = origin.to_owned()+"?search="+&search_encode(thing);
+  let uri = uri_str.parse().unwrap();
+  println!("Making request to: {}", &uri_str);
+  let req = Request::new(Method::Get, uri);
+
+
+
+  let work = client.request(req).and_then(|res| {
+    println!("Response Code: {}", res.status());
+    match res.headers().get_raw("Location") {
+      None =>  {
+        println!("Got no redirect");
+      },
+      Some(dest) => {
+        println!("Got redirect");
+        let d = String::from_utf8(dest.one().unwrap().to_vec()).unwrap(); // lovely
+        return future::ok::<_,Error>(Some(String::from(origin)+&d)).boxed()
+      }
+    };
+
+    // Ok we didn't get any precice match so we do some more investigation of what we have
+    res.body().fold(Vec::new(), |mut v, chunk| {
+      // translate the chunks into a vector of chunks
+      v.extend(&chunk[..]);
+      future::ok::<_, Error>(v)
+    }).and_then(|chunks| {
+      // translate the vector of chunks into a string
+      let s = String::from_utf8(chunks).unwrap();
+      if got_search_results_from_remote(&s) { return future::ok::<_, Error>(Some(uri_str)) }
+      future::ok::<_, Error>(None)
+    }).boxed()
+  });
+
+  return core.run(work).unwrap();
+}
+
+fn search_encode(thing: &str) -> String {
+  thing.replace(" ","+")
+}
+
+/// Private: Vanillagaming doesn't bother with HTTP return codes so we need to do
+/// some searching, can't be bothered with a proper HTML parser so we just do a quick string search.
+fn got_search_results_from_remote(body: &String) -> bool {
+  !body.contains("No results for ")
+}
